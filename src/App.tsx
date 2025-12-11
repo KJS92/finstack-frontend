08b1ae0f4c1d74ff53963e0cd34984d32c1fb2dc
KJS92 <reporting.aarmart@gmail.com>
2025-12-11 17:31:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import Accounts from './pages/Accounts';
+import PasswordReset from './pages/PasswordReset';
+import UpdatePassword from './pages/UpdatePassword';
 import './App.css';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/accounts" element={<Accounts />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
+          <Route path="/update-password" element={<UpdatePassword />} />
         </Routes>
       </div>
     </Router>
